465e8fca727e78bc805e7b7de2e0d1498df7fdd2
Patricio1207 <patriciovasquez1205@gmail.com>
2023-07-09 09:45:55 -0500

    agregar el src

---
+++ src/main/java/ec/edu/espol/proyecto1p/Proyecto1p.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.proyecto1p;
+
+/**
+ *
+ * @author Patricio Vásquez
+ */
+public class Proyecto1p {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
